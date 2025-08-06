d132a33ea2838b9fbabaf2ba1b1fac0cd6364d94
dlokut <dlokutijevski8@gmail.com>
2025-08-06 10:12:38 +0000

    Added code for responding with 200

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,21 @@
 using System.Net;
 using System.Net.Sockets;
 
+const int HTTP_OK = 200;
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket socket = server.AcceptSocket(); // wait for client
+
+int responseCode = HTTP_OK;
+byte[] responseCodeBytes = BitConverter.GetBytes(responseCode);
+
+int bytesSent = 0;
+while (bytesSent < responseCodeBytes.Length)
+{
+    bytesSent += socket.Send(responseCodeBytes, bytesSent,
+        responseCodeBytes.Length - bytesSent, SocketFlags.None);
+}
