4854530bac89997b593e9abe8e5ea48c8bfe4ddd
dlokut <dlokutijevski8@gmail.com>
2025-08-06 10:18:25 +0000

    changed to send proper http status

--- src/Server.cs
+++ src/Server.cs
@@ -1,7 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-const int HTTP_OK = 200;
+const string HTTP_OK = "HTTP/1.1 200 OK\r\n\r\n";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -10,8 +11,7 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Socket socket = server.AcceptSocket(); // wait for client
 
-int responseCode = HTTP_OK;
-byte[] responseCodeBytes = BitConverter.GetBytes(responseCode);
+byte[] responseCodeBytes = Encoding.UTF8.GetBytes(HTTP_OK);
 
 int bytesSent = 0;
 while (bytesSent < responseCodeBytes.Length)
